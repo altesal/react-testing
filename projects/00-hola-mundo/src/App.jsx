bd83332e2227da86bba0d6363abbc3e1ed7b411a
mplTrial <labsMPL@outlook.com>
2025-10-15 09:24:27 +0200

    00- TwitterCard. Pasar función como prop

--- projects/00-hola-mundo/src/App.jsx
+++ projects/00-hola-mundo/src/App.jsx
@@ -2,14 +2,16 @@ import './App.css'
 import { TwitterFollowCard } from './TwitterFollowCard.jsx'
 
 export function App() {
+    const format = (userName) => `@${userName}`
+    
     return(
-        <>
-        <TwitterFollowCard userName="calebporzio" name="MIguel García" />
-       <TwitterFollowCard userName="calebporzio" name="MIguel García" />
-       <TwitterFollowCard userName="calebporzio" name="MIguel García" />
-       <TwitterFollowCard userName="calebporzio" name="MIguel García" />
-       <TwitterFollowCard userName="calebporzio" name="MIguel García" />
-       <TwitterFollowCard userName="calebporzio" name="MIguel García" />
+       <>
+        <TwitterFollowCard formatUserName={format} isFollowing={true} userName="calebporzio" name="MIguel García" />
+        <TwitterFollowCard formatUserName={format} isFollowing={true} userName="pheralb" name="Bart" />
+        <TwitterFollowCard formatUserName={format} isFollowing={false} userName="midudev" name="Elon" />
+        <TwitterFollowCard formatUserName={format} isFollowing userName="vxnder" name="MIguel García" />
+        <TwitterFollowCard formatUserName={format} isFollowing userName="calebporzio" name="MIguel García" />
+        <TwitterFollowCard formatUserName={format} isFollowing userName="calebporzio" name="MIguel García" />
        </>
     )
 }
--- projects/00-hola-mundo/src/TwitterFollowCard.jsx
+++ projects/00-hola-mundo/src/TwitterFollowCard.jsx
@@ -1,12 +1,16 @@
-export function TwitterFollowCard ({userName, name, isFollowing}) {
-const imageSrc = `https://unavatar.io/github/${userName}`
+export function TwitterFollowCard ({formatUserName, userName, name, isFollowing}) {
+//const imageSrc = `https://unavatar.io/github/${userName}`
    return (
     <article className='tw-followCard'>
     <header className='tw-followCard-header'>
-        <img alt="El avatar de un invitado "  src={imageSrc} className='tw-followCard-avatar' /> 
+        <img 
+            alt="El avatar de un invitado "  
+            //src={imageSrc}
+            src={`https://unavatar.io/github/${userName}`} 
+            className='tw-followCard-avatar' /> 
         <div className='tw-followCard-info'>
             <strong>{name}</strong>
-            <span>@{userName}</span>
+            <span className="tw-followCard-infoUserName">{formatUserName(userName)}</span>
         </div>
     </header>
     <aside>
