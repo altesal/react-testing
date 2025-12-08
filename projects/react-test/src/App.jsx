0cbb4df09c50da8a491b699b78661f45890e769a
mplTrial <labsMPL@outlook.com>
2025-12-08 20:46:45 +0100

    Separamos la llamada a la imagen en otro servicio

--- projects/react-test/src/App.jsx
+++ projects/react-test/src/App.jsx
@@ -1,40 +1,21 @@
 import { useEffect, useState } from "react";
 import './App.css'; 
 import {getRandomFact} from './services/facts'
-
-
-const CAT_ENDPOINT_IMAGE_URL = 'https://cataas.com/cat/says/${firstWord}?size=50&color=red&json=true';
+import { getImage } from "./services/image";
 
 export function App() {
 const [fact, setFact] = useState();
 const [imageUrl, setImageUrl] = useState('');
 
-
 useEffect(() => {
   if (!fact) return;
-   const firstWord = fact.split(' ')[0];
-        const threeword = fact.split(' ').slice(0,3).join(' ');
-        //MDN (Mozilla Developer Network) para JavaScript es la plataforma de documentación de referencia gratuita y oficial de Mozilla
-        console.log(firstWord);
-        console.log(threeword);
-        
-  fetch(CAT_ENDPOINT_IMAGE_URL.replace('${firstWord}', threeword))
-          .then(res => res.json())
-          .then(response => {
-            console.log(response);
-            const {url} = response;  
-            setImageUrl(url);
-            console.log(url);
-            
-          });
+  const firstWord = fact.split(' ')[0];
+  getImage(firstWord).then(newUrlImage => setImageUrl(newUrlImage)) 
 }, [fact]);
 
-//Es bueno que un efecto tenga una sola responsabilidad. Recuperar la cita al renderizar la pagina
-// Recuperar la cita antes de cargar la página
-  useEffect( () => {
-    //getRandomFact().then(setFact)  //Azúcar sintáctico. Es lo mismo que la siguiente línea
-    getRandomFact().then(newFact => setFact(newFact))
-  }, [])
+useEffect( () => {
+  getRandomFact().then(newFact => setFact(newFact))
+}, [])
 
 const handleClick = async () => {
   const newFact = await getRandomFact()
@@ -45,20 +26,13 @@ return (
     <main  >
         <h1>App de gatitos Estilos verticales</h1>
           <button onClick={handleClick}>Get new fact</button>
-
           {fact && <p>{fact}</p> }
           {imageUrl && <img src={imageUrl} alt={`Imagen extraida usando las primeras palabras del hecho: ${fact}`} /> }
-        
-
         <h1>App de gatitos - Estilos horizontales</h1>
-        
-        
         <section>
           {fact && <p>{fact}</p> }
           {imageUrl && <img src={imageUrl} alt={`Imagen extraida usando las primeras palabras del hecho: ${fact}`} /> }
         </section>
-
-
     </main>
   );
 }
--- projects/react-test/src/services/facts.js
+++ projects/react-test/src/services/facts.js
@@ -2,15 +2,6 @@ import { requestFormReset } from "react-dom";
 
 const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact';
 
-// export const getRandomFact_ = () => {
-//    return fetch(CAT_ENDPOINT_RANDOM_FACT)
-//       .then(res => res.json())
-//       .then(data => {
-//         const {fact}  = data;
-//         return fact;   
-//         })
-//     }
-
 export const getRandomFact = async () => {
    const res =  await fetch(CAT_ENDPOINT_RANDOM_FACT)
    const data = await res.json()
