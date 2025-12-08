ac8613df754b2cfc175ffff1292ae8f2e7a460dc
mplTrial <labsMPL@outlook.com>
2025-12-08 19:08:01 +0100

    Ejecutar con async-await en vez de con promesas

--- projects/react-test/src/services/facts.js
+++ projects/react-test/src/services/facts.js
@@ -1,11 +1,19 @@
+import { requestFormReset } from "react-dom";
+
 const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact';
 
-export const getRandomFact = () => {
-   return fetch(CAT_ENDPOINT_RANDOM_FACT)
-      .then(res => res.json())
-      .then(data => {
-        const {fact}  = data;
-        return fact;   
-        })
+// export const getRandomFact_ = () => {
+//    return fetch(CAT_ENDPOINT_RANDOM_FACT)
+//       .then(res => res.json())
+//       .then(data => {
+//         const {fact}  = data;
+//         return fact;   
+//         })
+//     }
 
-    }
+export const getRandomFact = async () => {
+   const res =  await fetch(CAT_ENDPOINT_RANDOM_FACT)
+   const data = await res.json()
+   const { fact } = data 
+   return fact
+}
