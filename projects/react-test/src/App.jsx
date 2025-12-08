ef83c02020fa8acd94b6b5d0c6bfa6cd15779670
mplTrial <labsMPL@outlook.com>
2025-12-08 21:02:56 +0100

    customHook useCatImage

--- projects/react-test/src/App.jsx
+++ projects/react-test/src/App.jsx
@@ -3,15 +3,27 @@ import './App.css';
 import {getRandomFact} from './services/facts'
 import { getImage } from "./services/image";
 
+
+//CustomHook para traer la imagen del gato
+//Necesitamos el estado y el efecto
+//También necesitábamos el fact -la curiosidad del gato-
+//Diferencia con una función normal y un customHook? En un customHook podemos usar 
+// todos los hooks de React (en funciones nop)
+function useCatImage ({fact}) {
+  const [imageUrl, setImageUrl] = useState('');
+  useEffect(() => {
+    if (!fact) return;
+    const firstWord = fact.split(' ')[0];
+    getImage(firstWord).then(newUrlImage => setImageUrl(newUrlImage)) 
+  }, [fact]);
+  return  {imageUrl}
+}
+
+
 export function App() {
 const [fact, setFact] = useState();
-const [imageUrl, setImageUrl] = useState('');
+const {imageUrl} = useCatImage({fact})
 
-useEffect(() => {
-  if (!fact) return;
-  const firstWord = fact.split(' ')[0];
-  getImage(firstWord).then(newUrlImage => setImageUrl(newUrlImage)) 
-}, [fact]);
 
 useEffect( () => {
   getRandomFact().then(newFact => setFact(newFact))
