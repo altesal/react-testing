6447ab443be9990b0cf0686567cfc2b35377c743
mplTrial <labsMPL@outlook.com>
2025-12-09 08:39:08 +0100

    CustomHook que ejecuta un feth y actualiza un estado

--- projects/react-test/src/App.jsx
+++ projects/react-test/src/App.jsx
@@ -1,23 +1,33 @@
 import { useEffect, useState } from "react";
 import './App.css'; 
 import {getRandomFact} from './services/facts'
-import { getImage } from "./services/image";
 import { useCatImage } from "./hooks/useCatImage";
 
 
+//Nuevo customHook: nos traemos el estado y su efecto
+const useCatFact = () => {
+  const [fact, setFact] = useState();
+  
+  //Dentro del customHook sí que podría tener sentido explicar la implementación (nombres...)
+  const refreshRandomFact = () => {
+    getRandomFact().then(newFact => setFact(newFact))
+  }
+  //Lo anterior es justo lo que queríamos que hiciera el sig. efecto, pero también el botón!
+  // recuperar la cita al cargar la página
+   useEffect( () => {
+    refreshRandomFact()
+   }, [])
+  
+   return { fact, refreshRandomFact }
+}
 
 export function App() {
-const [fact, setFact] = useState();
-const {imageUrl} = useCatImage({fact})
-
-
-useEffect( () => {
-  getRandomFact().then(newFact => setFact(newFact))
-}, [])
+  const { fact, refreshRandomFact } = useCatFact()
+  const { imageUrl } = useCatImage({fact})
 
 const handleClick = async () => {
-  const newFact = await getRandomFact()
-  setFact(newFact)
+  //const newFact = await getRandomFact()
+  refreshRandomFact()
 }
 
 return (
