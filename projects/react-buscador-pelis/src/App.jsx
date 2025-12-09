66c438e3f715adc4595c0ef8aa02fb5b50c90a71
mplTrial <labsMPL@outlook.com>
2025-12-09 17:21:56 +0100

    Versión inicial. Lectura de mock y renderizado de pelis sin lógica de búsqueda

---
+++ projects/react-buscador-pelis/src/App.jsx
@@ -0,0 +1,40 @@
+import './App.css'
+import responseMovies from './mocks/pelis.json'
+import withoutResults from './mocks/no-results.json'
+
+function App() {
+  const movies = responseMovies.Search
+  const hasMovies = movies?.length > 0 
+
+  return (
+    <div className="page"> 
+      <header>
+      <form className='form'>
+        <input placeholder='Avengers, Star Warss, The Matrix...'></input>
+        <button type='submit'>Buscar</button>
+      </form>
+      </header>
+
+      <main>
+        {
+          hasMovies
+          ? <ul>
+            {
+              movies.map( movie => (
+                <li key={movie.imdbID}>
+                  <h3> {movie.Title}</h3>
+                  <p>{movie.Year}</p>
+                  <img src={movie.Poster} alt={movie.Title} />
+                  </li>
+              ))
+            }
+          </ul> : (
+            <p>No se han encontrado películas para esta búsqeuda</p>
+          )
+        }
+      </main>
+    </div>
+  )
+}
+
+export default App
