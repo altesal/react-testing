001358a307b42861f42ac1cde47ecc51a972d24a
mplTrial <labsMPL@outlook.com>
2025-12-10 12:20:50 +0100

    FORMA CONTROLADA - ESTADO. Ejemplo de validaciones usando Estado

--- projects/react-buscador-pelis/src/App.jsx
+++ projects/react-buscador-pelis/src/App.jsx
@@ -1,21 +1,38 @@
 import './App.css'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { Movies } from './components/Movies'
 import { useMovies } from './hooks/useMovies'
 
 function App() {
   const { movies } = useMovies()
   const [query, setQuery] = useState('')
+  const [error, setError] = useState('')
   
-  console.log("Uso de estado es más lento. RENDER cada vez que cambia un texto...")
-
+  
   const handleSubmit = (event) => {
     event.preventDefault()
     console.log(query)
   }
 
   const handleChange = (event) => {
-    setQuery(event.target.value)
+    //Validar (Ojo, podría hacerse perfectamente de FORMA NO CONTROLADA...)
+    //Ojo, estados son asíncronos
+    const newQuery = event.target.value
+    setQuery(newQuery)
+    if (newQuery === ''){
+      setError('No se pueden buscar pelíoculas vacías')
+      return
+    }  
+    if (newQuery.match(/^\d+$/)){
+      setError('No se puede buscar películas con un número')
+      return
+    }    
+    if (newQuery.length < 3) {
+      setError('La búsqueda debe tener al menos 3 caracteres')
+      return
+    }    
+    setError(null)
+
   }
 
   return (
@@ -25,6 +42,7 @@ function App() {
         <input onChange={handleChange} value={query} name='query' placeholder='Avengers, Star Warss, The Matrix...'></input>
         <button type='submit'>Buscar</button>
       </form>
+      {error && <p style={{color:'red'}}>{error}</p>}
       </header>
 
       <main>
