2a3280624c859851649af525b6e6930a6a7c78cb
mplTrial <labsMPL@outlook.com>
2025-12-10 12:50:29 +0100

    FORMA CONTROLADA - ESTADO. Extraer la lógica de validación de busqueda a un customHook

--- projects/react-buscador-pelis/src/App.jsx
+++ projects/react-buscador-pelis/src/App.jsx
@@ -3,43 +3,51 @@ import { useState, useEffect } from 'react'
 import { Movies } from './components/Movies'
 import { useMovies } from './hooks/useMovies'
 
+export function useSearch () {
+  const [search, updateSearch] = useState('')
+  const [error, setError] = useState(null)
+ 
+  useEffect( () => {
+    if (search === ''){
+      setError('No se pueden buscar pelíoculas vacías')
+      return
+    }
+
+    if (search.match(/^\d+$/)){
+      setError('No se puede buscar películas con un número')
+      return
+    }    
+    
+    if (search.length < 3) {
+      setError('La búsqueda debe tener al menos 3 caracteres')
+      return
+    }    
+    
+    setError(null)
+  } , [search])
+
+  return { search, updateSearch, error }
+}
+
 function App() {
   const { movies } = useMovies()
-  const [query, setQuery] = useState('')
-  const [error, setError] = useState('')
+  const {search, updateSearch, error } = useSearch()
   
   
   const handleSubmit = (event) => {
     event.preventDefault()
-    console.log(query)
+    console.log({search})
   }
 
   const handleChange = (event) => {
-    //Validar (Ojo, podría hacerse perfectamente de FORMA NO CONTROLADA...)
-    //Ojo, estados son asíncronos
-    const newQuery = event.target.value
-    setQuery(newQuery)
-    if (newQuery === ''){
-      setError('No se pueden buscar pelíoculas vacías')
-      return
-    }  
-    if (newQuery.match(/^\d+$/)){
-      setError('No se puede buscar películas con un número')
-      return
-    }    
-    if (newQuery.length < 3) {
-      setError('La búsqueda debe tener al menos 3 caracteres')
-      return
-    }    
-    setError(null)
-
+    updateSearch(event.target.value)
   }
 
   return (
     <div className="page"> 
       <header>
       <form className='form' onSubmit={handleSubmit}>
-        <input onChange={handleChange} value={query} name='query' placeholder='Avengers, Star Warss, The Matrix...'></input>
+        <input onChange={handleChange} value={search} name='query' placeholder='Avengers, Star Warss, The Matrix...'></input>
         <button type='submit'>Buscar</button>
       </form>
       {error && <p style={{color:'red'}}>{error}</p>}
