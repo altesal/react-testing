236bb668c6abcc7ee696f2fed93a18ec8ccf4023
mplTrial <labsMPL@outlook.com>
2025-12-10 11:36:01 +0100

    Recuperar todos los valores de un Form en un objeto - ObjectfromEntries

--- projects/react-buscador-pelis/src/App.jsx
+++ projects/react-buscador-pelis/src/App.jsx
@@ -1,28 +1,26 @@
 import './App.css'
-import { useRef } from 'react'
 import { Movies } from './components/Movies'
 import { useMovies } from './hooks/useMovies'
 
-
 function App() {
   const { movies } = useMovies()
-  const inputRef = useRef() //Valor inicial dentro del useRef, en este caso vacío
-
+  
 const handleSubmit = (event) => {
   event.preventDefault()
-  //cada vez que haya un clic, sacamos el valor de la referencia
-  //al acceder a una referencia, hacerlo desde la propiedad (nativa) current
-  const inputElement = inputRef.current.value
-  //alert(inputElement)
-  console.log(inputElement)
+  const fields_all = Object.fromEntries(new window.FormData(event.target))
+  console.log(fields_all)
+  const fields = new window.FormData(event.target)
+  const query = fields.get('query')
+  console.log(query)
 }
 
   return (
     <div className="page"> 
       <header>
       <form className='form' onSubmit={handleSubmit}>
-        <input ref={inputRef}  placeholder='Avengers, Star Warss, The Matrix...'></input>
-        <button   type='submit'>Buscar</button>
+        <input name='query' placeholder='Avengers, Star Warss, The Matrix...'></input>
+        <input name='otracosa' placeholder='Avengers, Star Warss, The Matrix...'></input>
+        <button type='submit'>Buscar</button>
       </form>
       </header>
 
