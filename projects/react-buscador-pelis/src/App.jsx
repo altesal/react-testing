439d33d416c52b071e5a3119ad24e25b3110f745
mplTrial <labsMPL@outlook.com>
2025-12-10 11:20:27 +0100

    Capturar valor del input con useRef

--- projects/react-buscador-pelis/src/App.jsx
+++ projects/react-buscador-pelis/src/App.jsx
@@ -1,17 +1,26 @@
 import './App.css'
+import { useRef } from 'react'
 import { Movies } from './components/Movies'
 import { useMovies } from './hooks/useMovies'
 
 
 function App() {
   const { movies } = useMovies()
+  const inputRef = useRef() //Valor inicial dentro del useRef, en este caso vacío
+
+const handleSubmit = () => {
+  //cada vez que haya un clic, sacamos el valor de la referencia
+  //al acceder a una referencia, hacerlo desde la propiedad (nativa) current
+  const inputElement = inputRef.current.value
+  alert(inputElement)
+}
 
   return (
     <div className="page"> 
       <header>
-      <form className='form'>
-        <input placeholder='Avengers, Star Warss, The Matrix...'></input>
-        <button type='submit'>Buscar</button>
+      <form className='form' onSubmit={handleSubmit}>
+        <input ref={inputRef}  placeholder='Avengers, Star Warss, The Matrix...'></input>
+        <button   type='submit'>Buscar</button>
       </form>
       </header>
 
