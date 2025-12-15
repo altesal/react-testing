7f6b8ceb5ae93af34c594b289a01b7f76837f94e
mplTrial <labsMPL@outlook.com>
2025-12-15 16:24:20 +0100

    Creamos servicio movies.js

--- projects/react-buscador-pelis/src/hooks/useMovies.js
+++ projects/react-buscador-pelis/src/hooks/useMovies.js
@@ -1,33 +1,13 @@
-import withResults from '../mocks/pelis.json'
-import withoutResults from '../mocks/no-results.json'
 import { useState } from 'react'
+import { searchMovies } from '../services/movies'
 
-//customHook que haga el fetching de datos películas, del estado... 
 export function useMovies ({search}) {
-  const [responseMovies, setResponseMovies] = useState([])
+  const [movies, setMovies] = useState([])
 
-  const movies = responseMovies.Search
-  //Mapeos para evitar que los componentes dependan mucho del contrato de la API
-  const mappedMovies = movies?.map(movie => ({
-      id: movie.imdbID,
-      title: movie.Title,
-      year : movie.Year,
-      poster: movie.Poster
-  }))
-
-  const getMovies = () => {
-    if (search) {
-      //setResponseMovies(withResults)
-      
-      fetch(`httpS://www.omdbapi.com/?apikey=f129656a&s=${search}`)
-      .then(res => res.json())
-      .then(json => {
-        setResponseMovies(json)
-    })
-    }  else {
-      setResponseMovies(withoutResults)
-    }
+  const getMovies = async () => {
+   const newMovies = await searchMovies({search})
+   setMovies(newMovies)
   }
 
-  return {movies: mappedMovies, getMovies}
+  return {movies, getMovies}
 }
