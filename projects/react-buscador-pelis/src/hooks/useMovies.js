154b4742d66b87cf93b785e71be697386b6fdb35
mplTrial <labsMPL@outlook.com>
2025-12-15 22:22:13 +0100

    UseRef para guardar la búsqueda anterior y evitar llamadas innecesarias

--- projects/react-buscador-pelis/src/hooks/useMovies.js
+++ projects/react-buscador-pelis/src/hooks/useMovies.js
@@ -1,15 +1,19 @@
-import { useState } from 'react'
+import { useRef,  useState } from 'react'
 import { searchMovies } from '../services/movies'
 
 export function useMovies ({search}) {
   const [movies, setMovies] = useState([])
   const [loading, setLoading] = useState(false)
   const [error, setError] = useState(null)
+  const previousSearch = useRef(search) //UseRef para guardar la búsqueda anterior y evitar llamadas innecesarias
  
-  const getMovies = async () => {
-     try {
+ const getMovies = async () => {
+  if (search === previousSearch.current) return //Si la búsqueda es igual a la anterior, no hacer nada
+
+  try {
       setLoading(true)
       setError(null)
+      previousSearch.current = search
       const newMovies = await searchMovies({search})
       setMovies(newMovies)
     }catch (e) {
