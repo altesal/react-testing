543891cb0a6e66ebcb710736c9abe5d1012f1517
mplTrial <labsMPL@outlook.com>
2025-12-15 15:46:59 +0100

    Fetch de datos con el texto de la caja de búsqueda Search

--- projects/react-buscador-pelis/src/hooks/useMovies.js
+++ projects/react-buscador-pelis/src/hooks/useMovies.js
@@ -1,8 +1,11 @@
-import responseMovies from '../mocks/pelis.json'
+import withResults from '../mocks/pelis.json'
 import withoutResults from '../mocks/no-results.json'
+import { useState } from 'react'
 
 //customHook que haga el fetching de datos películas, del estado... 
-export function useMovies () {
+export function useMovies ({search}) {
+  const [responseMovies, setResponseMovies] = useState([])
+
   const movies = responseMovies.Search
   //Mapeos para evitar que los componentes dependan mucho del contrato de la API
   const mappedMovies = movies?.map(movie => ({
@@ -12,5 +15,19 @@ export function useMovies () {
       poster: movie.Poster
   }))
 
-  return {movies: mappedMovies}
+  const getMovies = () => {
+    if (search) {
+      //setResponseMovies(withResults)
+      
+      fetch(`httpS://www.omdbapi.com/?apikey=f129656a&s=${search}`)
+      .then(res => res.json())
+      .then(json => {
+        setResponseMovies(json)
+    })
+    }  else {
+      setResponseMovies(withoutResults)
+    }
+  }
+
+  return {movies: mappedMovies, getMovies}
 }
